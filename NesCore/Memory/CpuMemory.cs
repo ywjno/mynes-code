cffdf25ce9859fcc683513753f329389cf2140f1
beannaich <beannaich@a07de2b8-32d0-4d93-a447-4c04374800b0>
2012-08-03 19:39:16 +0000

    Added preliminary board support

--- NesCore/Memory/CpuMemory.cs
+++ NesCore/Memory/CpuMemory.cs
@@ -2,8 +2,22 @@
 {
     public class CpuMemory : Memory
     {
+        private byte[] ram = new byte[2048];
+        private byte[] prg;
+
         public CpuMemory()
             : base(0xFFFF)
-        { }
+        {
+            Hook(0x0000, 0x2000, PeekRam, PokeRam);
+        }
+
+        private byte PeekRam(int address)
+        {
+            return ram[address & 0x7FF];
+        }
+        private void PokeRam(int address, byte data)
+        {
+            ram[address & 0x7FF] = data;
+        }
     }
 }
--- NesCore/Memory/Memory.cs
+++ NesCore/Memory/Memory.cs
@@ -1,75 +1,59 @@
 ﻿namespace MyNes.Core
 {
-   public abstract class Memory
+    public abstract class Memory
     {
-       public Memory(int capacity)
-       {
-           peek = new PeekRegister[capacity + 1];
-           poke = new PokeRegister[capacity + 1];
-           this.mask = capacity;
-           this.Map(0, capacity, PeekStub, PokeStub);
-       }
-       private PeekRegister[] peek;
-       private PokeRegister[] poke;
-       int mask;
+        private PeekRegister[] peek;
+        private PokeRegister[] poke;
+        private int mask;
 
-       public byte this[int addr]
-       {
-           get
-           {
-               return peek[addr &= mask](addr);
-           }
-           set
-           {
-               poke[addr &= mask](addr, value);
-           }
-       }
-       
-       private byte PeekStub(int addr)
-       {
-           return 0;
-       }
-       private void PokeStub(int addr, byte data)
-       {
-       }
+        public byte this[int addr]
+        {
+            get { return peek[addr & mask](addr); }
+            set { poke[addr & mask](addr, value); }
+        }
 
-       public void Map(int addr, PeekRegister peek)
-       {
-           this.peek[addr] = peek;
-       }
-       public void Map(int addr, PokeRegister poke)
-       {
-           this.poke[addr] = poke;
-       }
-       public void Map(int addr, PeekRegister peek, PokeRegister poke)
-       {
-           this.peek[addr] = peek;
-           this.poke[addr] = poke;
-       }
-       public void Map(int addr, int last, PeekRegister peek)
-       {
-           do
-           {
-               this.peek[addr] = peek;
-           }
-           while (addr++ != last);
-       }
-       public void Map(int addr, int last, PokeRegister poke)
-       {
-           do
-           {
-               this.poke[addr] = poke;
-           }
-           while (addr++ != last);
-       }
-       public void Map(int addr, int last, PeekRegister peek, PokeRegister poke)
-       {
-           do
-           {
-               this.peek[addr] = peek;
-               this.poke[addr] = poke;
-           }
-           while (addr++ != last);
-       }
+        public Memory(int capacity)
+        {
+            this.peek = new PeekRegister[capacity + 1];
+            this.poke = new PokeRegister[capacity + 1];
+            this.mask = capacity;
+
+            this.Hook(0, capacity, PeekStub, PokeStub);
+        }
+
+        private byte PeekStub(int addr) { return 0; }
+        private void PokeStub(int addr, byte data) { }
+
+        public void Hook(int addr, PeekRegister peek)
+        {
+            this.peek[addr] = peek;
+        }
+        public void Hook(int addr, PokeRegister poke)
+        {
+            this.poke[addr] = poke;
+        }
+        public void Hook(int addr, PeekRegister peek, PokeRegister poke)
+        {
+            this.peek[addr] = peek;
+            this.poke[addr] = poke;
+        }
+        public void Hook(int addr, int last, PeekRegister peek)
+        {
+            for (int i = addr; i <= last; i++)
+                this.peek[i] = peek;
+        }
+        public void Hook(int addr, int last, PokeRegister poke)
+        {
+            for (int i = addr; i <= last; i++)
+                this.poke[i] = poke;
+        }
+        public void Hook(int addr, int last, PeekRegister peek, PokeRegister poke)
+        {
+            for (int i = addr; i <= last; i++)
+            {
+                this.peek[i] = peek;
+                this.poke[i] = poke;
+            }
+        }
     }
 }
