994a507974d8ee2916e19df9c50ff1bae8167072
beannaich <beannaich@a07de2b8-32d0-4d93-a447-4c04374800b0>
2012-08-03 19:39:16 +0000

    Added preliminary board support

---
+++ trunk/NesCore/Boards/Board.cs
@@ -0,0 +1,18 @@
+﻿namespace MyNes.Core.Boards
+{
+    public class Board
+    {
+        private byte[] chr;
+        private byte[] prg;
+        private int chrMask;
+        private int prgMask;
+
+        private byte PeekChr(int address) { return chr[DecodeChrAddress(address) & chrMask]; }
+        private byte PeekPrg(int address) { return prg[DecodePrgAddress(address) & prgMask]; }
+        private void PokeChr(int address, byte data) { }
+        private void PokePrg(int address, byte data) { }
+
+        protected virtual int DecodeChrAddress(int address) { return address; }
+        protected virtual int DecodePrgAddress(int address) { return address; }
+    }
+}
