4d2836d750b1774ddaa6ae01cbb89338f30530ae
beannaich <beannaich@a07de2b8-32d0-4d93-a447-4c04374800b0>
2012-08-04 15:22:02 +0000

    Updated CPU syntax

--- trunk/NesCore/CPU/CPU.cs
+++ trunk/NesCore/CPU/CPU.cs
@@ -2,10 +2,10 @@
 {
     public class CPU
     {
+        private Flags sr;
         private Register aa;
         private Register pc;
         private Register sp;
-        private SRRegister regSR;
         private byte a;
         private byte x;
         private byte y;
