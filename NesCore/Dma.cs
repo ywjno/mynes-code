72f54585baf01fb69d59531761d3705703e86c37
beannaich <beannaich@a07de2b8-32d0-4d93-a447-4c04374800b0>
2012-08-05 16:29:50 +0000

    Added Dma.cs, PPU/Ppu.cs

---
+++ NesCore/Dma.cs
@@ -0,0 +1,36 @@
+﻿namespace MyNes.Core
+{
+    // Emulates the undocumented direct memory access controller in the 2A03/2A07
+    public class Dma
+    {
+        private bool step;
+        private byte data;
+        private int addr;
+        private int size;
+
+        public void OamTransfer(int address, byte data)
+        {
+            if (size != 0)
+                return; // transfer already occuring
+
+            step = false;
+            addr = (data << 8);
+            size = 256;
+        }
+
+        public void Update()
+        {
+            if (step = !step)
+            {
+                data = NesCore.CpuMemory[addr];
+            }
+            else
+            {
+                NesCore.CpuMemory[0x2004] = data;
+
+                addr = (++addr) & 0xFFFF;
+                size = (--size) & 0xFFFF;
+            }
+        }
+    }
+}
