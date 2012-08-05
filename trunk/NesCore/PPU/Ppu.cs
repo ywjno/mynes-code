39835243442651227e819280e4eb7dcca2233f43
beannaich <beannaich@a07de2b8-32d0-4d93-a447-4c04374800b0>
2012-08-05 16:29:50 +0000

    Added Dma.cs, PPU/Ppu.cs

---
+++ trunk/NesCore/PPU/Ppu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NesCore.PPU
+{
+    // Emulates the RP2C02/RP2C07 graphics synthesizer
+    public class Ppu
+    {
+    }
+}
