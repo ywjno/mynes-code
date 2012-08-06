52887ca5809dc3c8e1009b7f5499b16d2f2d46ae
beannaich <beannaich@a07de2b8-32d0-4d93-a447-4c04374800b0>
2012-08-06 13:43:43 +0000

    worked on PPU more, still can't test without video output but it should be working

--- trunk/NesCore/IO/Output/IVideoDevice.cs
+++ trunk/NesCore/IO/Output/IVideoDevice.cs
@@ -6,7 +6,7 @@
         /// Render a frame into the screen, the ppu calls this when the buffer is ready to be presented
         /// </summary>
         /// <param name="ScreenBuffer">The screen buffer, size = 256 * 240, each value is int32-ARBG color</param>
-        void RenderFrame(int[] ScreenBuffer);
+        void RenderFrame(int[][] ScreenBuffer);
         /// <summary>
         /// Begin the rendering operation, Ppu calls this at the beginning of the frame
         /// </summary>
--- trunk/NesCore/PPU/Ppu.cs
+++ trunk/NesCore/PPU/Ppu.cs
@@ -3,20 +3,54 @@
     // Emulates the RP2C02/RP2C07 graphics synthesizer
     public class Ppu : ProcessorBase
     {
+        private Fetch fetch = new Fetch();
         private Scroll scroll = new Scroll();
+        private Sprite[] buffer = new Sprite[8];
         private Unit bkg = new Unit(272);
         private Unit spr = new Unit(256);
+        private bool nmi;
         private bool vbl;
         private byte chr;
+        private int clipping;
+        private int emphasis;
         private int hclock;
         private int vclock;
+        private int[] colors;
+        private int[][] screen;
 
         public Ppu(TimingInfo.Cookie cookie)
             : base(cookie)
         {
             timing.period = cookie.Master;
             timing.single = cookie.Gpu;
+
+            colors = new int[64];
+            screen = new int[240][];
+
+            for (int i = 0; i < 240; i++)
+                screen[i] = new int[256];
         }
+
+        private void FetchName_0()
+        {
+            fetch.addr = 0x2000 | (scroll.addr & 0xFFF);
+        }
+        private void FetchName_1() { fetch.name = NesCore.PpuMemory[fetch.addr]; }
+        private void FetchAttr_0()
+        {
+            fetch.addr = 0x23C0 | (scroll.addr & 0xC00) | (scroll.addr >> 4 & 0x38) | (scroll.addr >> 2 & 0x7);
+        }
+        private void FetchAttr_1() { fetch.attr = NesCore.PpuMemory[fetch.addr] >> ((scroll.addr >> 4 & 0x04) | (scroll.addr & 0x02)); }
+        private void FetchBit0_0()
+        {
+            fetch.addr = bkg.address | (fetch.name << 4) | 0 | (scroll.addr >> 12 & 7);
+        }
+        private void FetchBit0_1() { fetch.bit0 = NesCore.PpuMemory[fetch.addr]; }
+        private void FetchBit1_0()
+        {
+            fetch.addr = bkg.address | (fetch.name << 4) | 8 | (scroll.addr >> 12 & 7);
+        }
+        private void FetchBit1_1() { fetch.bit1 = NesCore.PpuMemory[fetch.addr]; }
 
         private byte Peek2002(int address)
         {
@@ -49,8 +83,26 @@
 
             return tmp;
         }
-        private void Poke2000(int address, byte data) { }
-        private void Poke2001(int address, byte data) { }
+        private void Poke2000(int address, byte data)
+        {
+            scroll.temp = (scroll.temp & ~0x0C00) | (data << 10 & 0x0C00);
+            scroll.step = (data & 0x04) != 0 ? 0x0020 : 0x0001;
+            spr.address = (data & 0x08) != 0 ? 0x1000 : 0x0000;
+            bkg.address = (data & 0x10) != 0 ? 0x1000 : 0x0000;
+            spr.rasters = (data & 0x20) != 0 ? 0x0010 : 0x0008;
+
+            nmi = (data & 0x80) != 0;
+        }
+        private void Poke2001(int address, byte data)
+        {
+            clipping = (data & 0x01) != 0 ? 0x30 : 0x3F;
+            emphasis = (data & 0xE0) << 1;
+
+            bkg.clipped = (data & 0x02) == 0;
+            spr.clipped = (data & 0x04) == 0;
+            bkg.enabled = (data & 0x08) != 0;
+            spr.enabled = (data & 0x10) != 0;
+        }
         private void Poke2003(int address, byte data) { }
         private void Poke2004(int address, byte data) { }
         private void Poke2005(int address, byte data)
@@ -78,8 +130,126 @@
             scroll.addr = (scroll.addr + scroll.step) & 0x7FFF;
         }
 
+        private void RenderPixel()
+        {
+            var bkgPixel = 0x3F00 | bkg.GetPixel(hclock, scroll.fine);
+            var sprPixel = 0x3F10 | spr.GetPixel(hclock);
+
+            // todo: handle priority and sprite-zero hit
+            var pixel = bkgPixel;
+
+            if ((pixel & 0x03) == 0)
+                pixel = 0x3F00;
+
+            screen[vclock][hclock] = colors[NesCore.PpuMemory[pixel]];
+        }
+        private void SynthesizeBkgPixels()
+        {
+            var pos = (hclock + 9) % 336;
+
+            for (int i = 0; i < 8 && pos < 256; i++, pos++, fetch.bit0 <<= 1, fetch.bit1 <<= 1)
+                bkg.pixels[pos] = (fetch.attr << 2 & 0xC) | (fetch.bit0 >> 7 & 0x1) | (fetch.bit1 >> 6 & 0x2);
+        }
+        private void SynthesizeSprPixels()
+        {
+            int pos = buffer[hclock >> 3 & 7].x;
+
+            for (int i = 0; i < 8 && pos < 256; i++, pos++, fetch.bit0 <<= 1, fetch.bit1 <<= 1)
+                spr.pixels[pos] = (fetch.attr << 2 & 0xC) | (fetch.bit0 >> 7 & 0x1) | (fetch.bit1 >> 6 & 0x2);
+        }
+
         public override void Update()
         {
+            if (vclock < 240 || vclock == 261)
+            {
+                if (bkg.enabled || spr.enabled)
+                {
+                    if (hclock < 256)
+                    {
+                        #region Bkg Fetches
+
+                        switch (hclock & 7)
+                        {
+                            case 0: FetchName_0(); break;
+                            case 1: FetchName_1(); break;
+                            case 2: FetchAttr_0(); break;
+                            case 3: FetchAttr_1(); scroll.ClockX(); break;
+                            case 4: FetchBit0_0(); break;
+                            case 5: FetchBit0_1(); break;
+                            case 6: FetchBit1_0(); break;
+                            case 7: FetchBit1_1(); SynthesizeBkgPixels(); break;
+                        }
+
+                        #endregion
+                    }
+                    else if (hclock < 320)
+                    {
+                        if (hclock == 256)
+                        {
+                            scroll.ResetX();
+                            scroll.ClockY();
+                        }
+
+                        if (hclock == 304 && vclock == 261)
+                            scroll.addr = scroll.temp;
+
+                        #region Spr Fetches
+
+                        switch (hclock & 7)
+                        {
+                            case 0: FetchName_0(); break;
+                            case 1: FetchName_1(); break;
+                            case 2: FetchAttr_0(); break;
+                            case 3: FetchAttr_1(); break;
+                            case 4: /*   Bit0   */ break;
+                            case 5: /*   Bit0   */ break;
+                            case 6: /*   Bit1   */ break;
+                            case 7: /*   Bit1   */ SynthesizeSprPixels(); break;
+                        }
+
+                        #endregion
+                    }
+                    else if (hclock < 336)
+                    {
+                        #region Bkg Fetches
+
+                        switch (hclock & 7)
+                        {
+                            case 0: FetchName_0(); break;
+                            case 1: FetchName_1(); break;
+                            case 2: FetchAttr_0(); break;
+                            case 3: FetchAttr_1(); scroll.ClockX(); break;
+                            case 4: FetchBit0_0(); break;
+                            case 5: FetchBit0_1(); break;
+                            case 6: FetchBit1_0(); break;
+                            case 7: FetchBit1_1(); SynthesizeBkgPixels(); break;
+                        }
+
+                        #endregion
+                    }
+                    else if (hclock < 340)
+                    {
+                        #region Dummy Fetches
+
+                        switch (hclock & 1)
+                        {
+                            case 0: FetchName_0(); break;
+                            case 1: FetchName_1(); break;
+                        }
+
+                        #endregion
+                    }
+                    else
+                    {
+                        // Idle cycle
+                    }
+                }
+                else
+                {
+                    // Rendering is off
+                }
+            }
+
             hclock++;
 
             if (hclock == 341)
@@ -88,19 +258,34 @@
                 vclock++;
 
                 if (vclock == 241)
+                {
                     vbl = true;
+
+                    if (nmi)
+                        NesCore.Cpu.NMI(true);
+                }
 
                 if (vclock == 262)
                 {
                     vclock = 0;
                     vbl = false;
+
+                    NesCore.VideoDevice.RenderFrame(screen);
                 }
             }
         }
 
-        public void Shutdown() { }
         public void Initialize() { }
+        public void Shutdown() { }
 
+        public class Fetch
+        {
+            public int addr;
+            public int attr;
+            public int bit0;
+            public int bit1;
+            public int name;
+        }
         public class Scroll
         {
             public bool swap;
@@ -137,11 +322,19 @@
                 addr = (addr & ~0x41F) | (temp & 0x41F);
             }
         }
+        public class Sprite
+        {
+            public byte y;
+            public byte name;
+            public byte attr;
+            public byte x;
+        }
         public class Unit
         {
-            public bool clipped;
-            public bool enabled;
-            public int address;
+            public bool  clipped;
+            public bool  enabled;
+            public int   address;
+            public int   rasters;
             public int[] pixels;
 
             public Unit(int capacity)
