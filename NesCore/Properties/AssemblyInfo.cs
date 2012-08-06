b626f3b872ed3ed6f71b87ac51bc3db898295d4e
beannaich <beannaich@a07de2b8-32d0-4d93-a447-4c04374800b0>
2012-08-06 15:54:40 +0000

    Renamed core project

--- NesCore/Properties/AssemblyInfo.cs
+++ NesCore/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NesCore")]
+[assembly: AssemblyTitle("MyNes.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NesCore")]
+[assembly: AssemblyProduct("MyNes.Core")]
 [assembly: AssemblyCopyright("Copyright © Ala I Hadid 2009 - 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
