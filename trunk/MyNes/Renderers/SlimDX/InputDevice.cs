9673b1c535345a65e685205d02b7899d2c55a59f
jegqamas <jegqamas@a07de2b8-32d0-4d93-a447-4c04374800b0>
2012-08-09 06:58:44 +0000

    Added input controls

---
+++ trunk/MyNes/Renderers/SlimDX/InputDevice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SlimDX;
+using SlimDX.DirectInput;
+
+namespace myNES
+{
+    public class InputDevice
+    {
+        private Device _device;
+        private DeviceType _type;
+        private KeyboardState _keyboardState;
+        private JoystickState _joystickState;
+
+        public DeviceType Type { get { return _type; } }
+        public KeyboardState KeyboardState { get { return _keyboardState; } }
+        public JoystickState JoystickState { get { return _joystickState; } }
+
+        public InputDevice(Device device)
+        {
+            _device = device;
+            if ((device.Information.Type & DeviceType.Keyboard) == DeviceType.Keyboard)
+                _type = DeviceType.Keyboard;
+            else if ((device.Information.Type & DeviceType.Joystick) == DeviceType.Joystick)
+                _type = DeviceType.Joystick;
+            else
+                _type = DeviceType.Other;
+        }
+
+        public void Update()
+        {
+            if (_device.Acquire().IsFailure)
+                return;
+
+            if (_device.Poll().IsFailure)
+                return;
+
+            if (_device.Acquire().IsSuccess)
+            {
+                if (_type == DeviceType.Keyboard)
+                    _keyboardState = ((Keyboard)_device).GetCurrentState();
+                else if (_type == DeviceType.Joystick)
+                    _joystickState = ((Joystick)_device).GetCurrentState();
+            }
+        }
+    }
+}
---
+++ trunk/MyNes/Renderers/SlimDX/TIMER.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using myNES.Core.Controls;
+
+namespace myNES
+{
+    public class TIMER : ITimer
+    {
+        private long _frequency;
+        public TIMER()
+        {
+            if (!QueryPerformanceFrequency(out this._frequency))
+            {
+                throw new Win32Exception();
+            }
+        }
+        public double GetCurrentTime()
+        {
+            long num;
+            QueryPerformanceCounter(out num);
+            return (((double)num) / ((double)this._frequency));
+        }
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryPerformanceCounter(out long lpPerformanceCount);
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryPerformanceFrequency(out long lpFrequency);
+    }
+}
