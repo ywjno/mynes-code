2765e9b7f79e62f3fefb6d373d044437f8ad67db
jegqamas <jegqamas@a07de2b8-32d0-4d93-a447-4c04374800b0>
2012-11-14 21:57:36 +0000

    Added mappers 17 and 18, started mapper 19

--- trunk/NesCore/Boards/Nintendo/MMC1.cs
+++ trunk/NesCore/Boards/Nintendo/MMC1.cs
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/*Written by Ala Ibrahim Hadid and Adam Becker*/
 using MyNes.Core.Types;
 namespace MyNes.Core.Boards.Nintendo
 {
--- trunk/NesCore/Boards/Nintendo/MMC2.cs
+++ trunk/NesCore/Boards/Nintendo/MMC2.cs
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/*Written by Adam Becker*/
 namespace MyNes.Core.Boards.Nintendo
 {
     [BoardName("MMC2", 9)]
